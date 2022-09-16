526ebc98c8b6bd5e36ef807a55bb24411dbe6394
Senavarici <sena.varicii@gmail.com>
2022-09-16 12:12:04 +0300

    created #3

--- veri_analizi.py
+++ veri_analizi.py
@@ -4,6 +4,28 @@ import re
 import sqlite3 as sqlite
 from email_validator import validate_email, EmailNotValidError
 
+db = input("isim gir: ")
+conn = sqlite.connect("{}.db".format(db))
+cs = conn.cursor()
+class Database:       
+    def create_table(self, name, datas):
+        self.name = name
+        crt = "create table if not exists {} (id integer primary key autoincrement".format(self.name)
+        for value in datas.values():
+            crt += ("," + str(value))
+        crt += ")"
+        cs.execute(crt)
+        conn.commit()      
+        
+    def insert_table(self,name, datas):
+        self.name = name
+        insrt = "insert into {} values (null".format(self.name)
+        for value in datas.values():
+            insrt += (","+ '"' + str(value) + '"') 
+        insrt += ')'
+        cs.execute(insrt)
+        conn.commit()
+
 def checkmail(email):
     try:
         valid = validate_email(email)
@@ -11,12 +33,11 @@ def checkmail(email):
     except EmailNotValidError as e:
         return False
 
-db = input("isim gir: ")
-conn = sqlite.connect("{}.db".format(db))
-cs = conn.cursor()
-cs.execute("create table if not exists data (id integer primary key autoincrement, url, email, phone)") 
+table = Database()
+table.create_table("data", {'column1':"url", 'column2':"email", 'column3':"phone"})
 url = input("site linki gir: ")
 urls = set()
+
 def AllLinks(url):   
     page = requests.get(url)
     soup = BeautifulSoup(page.text, 'html.parser')
@@ -28,18 +49,20 @@ def AllLinks(url):
         if str(i).startswith("https") == False and str(i).startswith("/"):
             urls.add(url+str(i))
             
-    cs.execute("create table if not exists code_200 (id integer primary key autoincrement, url)")      
-    cs.execute("create table if not exists code_404 (id integer primary key autoincrement, url)") 
-    conn.commit() 
+    table1 = Database()
+    table1.create_table("code_200",{'column1':"url"})
+    table2 = Database()
+    table2.create_table("code_404",{'column1':"url"})
+   
     for url in urls:        
         page2 = requests.get(url)
         soup = BeautifulSoup(page2.text, 'html.parser')
-        if page2.status_code == 200:           
-            cs.execute("insert into code_200 values (null,?)",[url])
-            conn.commit()      
-        else:           
-            cs.execute("insert into code_404 values (null,?)",[url])
-            conn.commit()
+        if page2.status_code == 200:   
+            table1 = Database()
+            table1.insert_table("code_200",{'column1':url})             
+        else:          
+            table2 = Database()
+            table2.insert_table("code_404",{'column1':url}) 
                   
         email = re.findall("((?:[\w\.\-\+\_]+)@(?:[a-zA-Z0-9\.\-]+))",str(soup)) 
         try:
@@ -51,9 +74,9 @@ def AllLinks(url):
             pass
 
         phone =re.findall("^(\d{4}\s?\(?\d{3}\)?[\s]\d{4})|(\d{1,2}\s?\(?\d{3}\)?[\s]\d{3}[\s]\d{2}[\s]\d{2})|(\d{3}\)?[\s]\d{1}[\s]\d{3})",str(soup))  
-        phone = [*set(phone)]   
-        cs.execute("insert into Data values (null,?,?,?)",(url,str(email),str(phone)))
-        conn.commit()
-             
+        phone = [*set(phone)] 
+        table = Database()
+        table.insert_table("data", {'column1':url, 'column2':email, 'column3':phone}) 
+        
 AllLinks(url)
 
